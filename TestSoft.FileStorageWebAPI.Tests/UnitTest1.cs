80259f0e8f15ec19f08793c4dcff5cb1daab5aaf
gregory <burtelgamerpro@gmail.com>
2024-12-06 01:29:55 +0600

    v1.5.1 - add test projects

---
+++ TestSoft.FileStorageWebAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestSoft.FileStorageWebAPI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
